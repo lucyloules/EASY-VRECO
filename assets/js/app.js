c3e2c703780f6c38ccf6ec7fd3e2901712c4cf2b
Lucyloules <lucia.lagos@gmail.com>
2018-02-16 16:48:23 -0300

    buscador y tarjeta no funciona aun

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
+
 var btnSearch = $('#btnSearch'); // Boton de busqueda 
 initJson();
 
 function initJson() {
-  fetch('https://pokeapi.co/api/v2/pokemon/')
+  /* 'https://pokeapi.co/api/v2/pokemon/?limit=721' */
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=721')
     .then(function(response) {
       // Turns the the JSON into a JS object
       return response.json();
@@ -24,5 +26,50 @@ function initJson() {
     });
 };
 
+// funcion de busqueda
+btnSearch.on('click', function(element) {
+  element.preventDefault();
+  var btnSearch = $('btnSearch').val();
+  let container = $('<div>').addClass('col-xs-12 col-md-4 cardItem');
+  fetch(`https://pokeapi.co/api/v2/pokemon/${btnSearch}/`) // busca por el nombre ingresado en el input
+    .then(function(response) {
+      // Turns the the JSON into a JS object
+      return response.json();
+    })
+    .then(function(data) {
+      let img = $('<img class = "imgPoke">').attr('src', `https://pokeapi.co/media/img/${data.id}.png`); 
+      let title = $('<h2 class="name">').text(data.name);
+      let height = $('<div class="col-md-4 caract"><p>').text('Altura: ' + data.height / 10 + ' m');
+      let weight = $('<div class="col-md-4 caract"><p>').text('Peso: ' + data.weight / 10 + ' kg');
 
+      container.append(title, img, height, weight);
+      // obtener la habilidad
+      data.abilities.forEach(function(element) {
+        if (element.is_hidden === true) {
+          container.append('<div class="col-md-5 caract"><p>Habilidad: ' + element.ability.name + '</p></div>');
+        }
+      });
+      $('#pokemons').html(container);
+      // para obtener la descripción
+      return fetch(`https://pokeapi.co/api/v2/pokemon-species/${data.id}/`);
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // para obtener el tipo de pokemon
+      data.genera.forEach(function(element) {
+        if (element.language.name === 'es') { // restringo a lenguaje español
+          let category = $('<div class="caractGenus col-md-6"><p>').text('Categoría: ' + element.genus);
+          container.append(category);
+        }
+      });
+      data.flavor_text_entries.forEach(function(element) {
+        if (element.language.name === 'es' && element.version.name === 'omega-ruby') {
+          $('.imgPoke').after('<p class ="description">' + element.flavor_text + '</p>');
+        }
+      });
+    }).catch(function(error) {
+    });
+});
 // https://pokeapi.co/docsv2/ 
